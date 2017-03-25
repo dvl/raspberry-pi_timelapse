2e0d76131656783ac13438453b1268733ae058e3
André Luiz <contato@xdvl.info>
2017-03-25 16:52:52 -0300

    Força resolução correta

--- render_video.sh
+++ render_video.sh
@@ -3,4 +3,4 @@
 
 TIMESTAMP=`date +%Y%m%d`
 
-ffmpeg -framerate 30 -pattern_type glob -i "images/img_*.png" -c:v libx264 -pix_fmt yuv420p "videos/${TIMESTAMP}.mp4"
+ffmpeg -framerate 30 -pattern_type glob -i "images/img_*.png" -c:v libx264 -pix_fmt yuv420p -vf scale=1024:768 "videos/${TIMESTAMP}.mp4"
