77c9cf7ea87a50e6fa60c0947abff0e129be7c25
André Luiz <contato@xdvl.info>
2017-03-25 01:39:24 -0300

    Helper scritps

---
+++ fetch_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -Cravzp pi@192.168.1.5:/home/pi/Projects/raspberry-pi_timelapse/images/ /home/andre/Projects/raspberry-pi_timelapse/images/
---
+++ render_video.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TIMESTAMP=`date +%Y%m%d`
+
+ffmpeg -r 120 -pattern_type glob -i "images/img_*.png" -vf format=yuv420p ${TIMESTAMP}.mp4
