9849cba05459f0b829502e4270bf4b69476a1e94
André Luiz <contato@xdvl.info>
2017-03-25 16:24:58 -0300

    better video render

--- render_video.sh
+++ render_video.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# https://trac.ffmpeg.org/wiki/Slideshow
 
 TIMESTAMP=`date +%Y%m%d`
 
-ffmpeg -r 120 -pattern_type glob -i "images/img_*.png" -vf format=yuv420p "videos/${TIMESTAMP}.mp4"
+ffmpeg -framerate 120 -pattern_type glob -i "images/img_*.png" -c:v libx264 -pix_fmt yuv420p "videos/${TIMESTAMP}.mp4"
