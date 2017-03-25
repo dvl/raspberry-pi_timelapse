1048c0e81608caa92778a609d4cfd74e8dcc7896
André Luiz <contato@xdvl.info>
2017-03-25 04:02:04 -0300

    fix path

--- render_video.sh
+++ render_video.sh
@@ -2,4 +2,4 @@
 
 TIMESTAMP=`date +%Y%m%d`
 
-ffmpeg -r 120 -pattern_type glob -i "images/img_*.png" -vf format=yuv420p ${TIMESTAMP}.mp4
+ffmpeg -r 120 -pattern_type glob -i "images/img_*.png" -vf format=yuv420p "videos/${TIMESTAMP}.mp4"
